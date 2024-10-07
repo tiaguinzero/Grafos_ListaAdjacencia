7c22b1846a8c53dfa3c1256df388243a5eff3b2c
jorge <162496379+cc24319@users.noreply.github.com>
2024-10-06 23:24:21 -0300

    Add files via upload
    mudancaTrajetoseEvitarRepeticao

--- Aeroporto.java
+++ Aeroporto.java
@@ -1,11 +1,18 @@
 class Aeroporto {
     String nome;
-    String codigo;
-    ListaEncadeada voos;  // Lista de voos que saem deste aeroporto
-
-    public Aeroporto(String nome, String codigo) {
+    int codigo;
+    ListaEncadeada voosChegando;  
+    ListaEncadeada voosPartindo; // Lista de voos que saem deste aeroporto
+    public Aeroporto(String nome, int codigo) {
         this.nome = nome;
         this.codigo = codigo;
-        this.voos = new ListaEncadeada();
+        this.voosChegando = new ListaEncadeada();
+        this.voosPartindo = new ListaEncadeada();
+    }
+
+    public Aeroporto(int codigo) {
+        this.codigo = codigo;
+        this.voosChegando = new ListaEncadeada();
+        this.voosPartindo = new ListaEncadeada();
     }
 }
--- ListaEncadeada.java
+++ ListaEncadeada.java
@@ -1,22 +1,43 @@
 class ListaEncadeada {
     Voo primeiro;
+    int tamanho;
 
     public ListaEncadeada() {
         this.primeiro = null;
     }
 
+
+    public Voo obterVooPorPosicao(int posicao) {
+        if (posicao < 0 || posicao >= tamanho) {
+            throw new IndexOutOfBoundsException("Posição fora dos limites da lista.");
+        }
+
+        Voo atual = primeiro;
+        int contador = 0;
+
+        // Percorre a lista até a posição desejada
+        while (contador < posicao && atual != null) {
+            atual = atual.proximo;
+            contador++;
+        }
+
+        return atual; // Retorna o voo na posição
+    }
+    
+
     // Adiciona um novo voo à lista
-    public void adicionarVoo(int indiceDestino, int numeroVoo) {
-        Voo novoVoo = new Voo(indiceDestino, numeroVoo);
+    public void adicionarVoo(int indiceOrigem, int indiceDestino, int numeroVoo) {
+        Voo novoVoo = new Voo(indiceOrigem, indiceDestino, numeroVoo);
         novoVoo.proximo = primeiro;
         primeiro = novoVoo;
+        tamanho++;
     }
 
     // Remove um voo pelo número
     public void removerVoo(int numeroVoo) {
         Voo atual = primeiro;
         Voo anterior = null;
-
+        tamanho--;
         while (atual != null) {
             if (atual.numeroVoo == numeroVoo) {
                 if (anterior == null) {
